cc5d98e1c95635e451babd4da177246c9674209e
vivek kapal <97444828+vivekkkapal@users.noreply.github.com>
2024-10-04 09:31:43 +0000

    card implement

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,37 @@
+import data from './data.json';
 
-const inputEL = document.querySelector("input");
-console.log(inputEL);
+// Dom selection
+const cardEL = document.querySelector('#row');
+const inputEL = document.querySelector('input');
 
-//add event lis
-document.addEventListener('keypress',function(){
-    console.log("event listner")
-})
+//add event listner
+document.addEventListener('keypress', (e) => {
+  if (e.key == '/') {
+    e.preventDefault();
+    inputEL.focus();
+  }
+});
+
+//show pokemon name 
+for (let pokemonobj of data) {
+   const div = document.createElement('div');
+   div.classList.add('col');
+   div.innerHTML = `<div class="card">
+            <img
+              src="${pokemonobj.image}"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">${pokemonobj.name}</h5>
+              <p class="card-text">
+              ${pokemonobj.description}
+              </p>
+            </div>
+          </div>`;
+
+    cardEL.appendChild(div);
+}
+
+function renderPokemons(list) {}
+
